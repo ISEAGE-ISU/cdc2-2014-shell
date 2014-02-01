eaf436ee4e33dd5069fe985d88919f87133dbde7
Alec Gleason <Alec.w.gleason@gmail.com>
2014-02-01 13:16:31 -0600

    Ugly Blog
    Got the posts aspect of the blog page complete. It just looks really ugly.
    
    Gleason

--- Primary/admin.py
+++ Primary/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Primary.models import Post
 
 # Register your models here.
+class postAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+
+admin.site.register(Post, postAdmin)
--- Primary/views.py
+++ Primary/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from Primary.models import Post
 
 # Create your views here.
+
+def blog(request):
+    blog_data = Post.objects.all()[:5]
+    context = { 'blog_data' : blog_data }
+    return render(request, 'Primary/index.html', context)
