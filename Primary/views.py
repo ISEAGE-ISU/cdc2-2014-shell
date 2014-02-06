61776ba2d48d06663dc225f14a9a714f855945e1
Alec Gleason <Alec.w.gleason@gmail.com>
2014-02-06 12:57:11 -0600

    Just changing some things
    Getting it all to work involves effort :'( Luckily, its fun!
    
    Gleason

--- Primary/views.py
+++ Primary/views.py
@@ -4,6 +4,6 @@ from Primary.models import Post
 # Create your views here.
 
 def blog(request):
-    blog_data = Post.objects.all()[:5]
+    blog_data = Post.objects.all().order_by('-date')[:5]
     context = { 'blog_data' : blog_data }
     return render(request, 'Primary/index.html', context)
